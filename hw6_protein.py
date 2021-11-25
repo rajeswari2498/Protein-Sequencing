69247082cadee39ce5f49a71f62116d441e67b80
rajeswari2498 <rajeswariyesasvini@msitprogram.net>
2021-11-25 18:47:11 +0530

    Completed the implementation of DNA to RNA

--- hw6_protein.py
+++ hw6_protein.py
@@ -31,7 +31,17 @@ Parameters: str ; int
 Returns: list of strs
 '''
 def dnaToRna(dna, startIndex):
-    return
+    list=[]
+    list1=[]
+    for i in range(startIndex,len(dna),3):
+        list.append(dna[i:i+3])
+        if dna[i:i+3]=="TAG" or dna[i:i+3]=="TAA" or dna[i:i+3]=="TGA":
+            break
+    for each in list:
+        each=each.replace("T","U")
+        list1.append(each)
+    # print(list1)
+    return list1     
 
 
 '''
@@ -191,6 +201,7 @@ def runFullProgram():
 # This code runs the test cases to check your work
 if __name__ == "__main__":
     test.testReadFile()
+    test.testDnaToRna()
     # print("\n" + "#"*15 + " WEEK 1 TESTS " +  "#" * 16 + "\n")
     # test.week1Tests()
     # print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
